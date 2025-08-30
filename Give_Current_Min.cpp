9cd729ec43b216fd13dc3f1abc82446a5ecce58f
MashrafiGit <mdmasrafi902@gmail.com>
2025-08-30 19:03:07 +0600

    print and delete current minimum value after doing some operation

---
+++ Give_Current_Min.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    int n;
+    cin >> n;
+
+    while (n--)
+    {
+        int val;
+        cin >> val;
+        pq.push(val);
+    }
+
+    int q;
+    cin >> q;
+
+    while (q--)
+    {
+        int x;
+        cin >> x;
+
+        if (x == 0)
+        {
+            int v;
+            cin >> v;
+            pq.push(v);
+            cout << pq.top() << endl;
+        }
+
+        else if (x == 1)
+        {
+            if (pq.empty())
+                cout << "Empty" << endl;
+            else
+                cout << pq.top() << endl;
+        }
+        else if (x == 2)
+        {
+            if (pq.empty())
+                cout << "Empty" << endl;
+            else
+            {
+                pq.pop();
+                if (pq.empty())
+                    cout << "Empty" << endl;
+                else
+                    cout << pq.top() << endl;
+            }
+        }
+    }
+
+    return 0;
+}
